d68364c46a6718307ab56f3bbb0d99fbd9922a2a
Ross Gonsolvis <gonsolvis@gmail.com>
2023-02-08 19:15:16 +0100

    will provide skeleton for activity

--- app.js
+++ app.js
@@ -12,14 +12,31 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+//2
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+
+ 
+//3
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+    .then(beersFromApi => {
+      console.log(beersFromApi[0])
+      res.render('random-beer', beersFromApi[0]);
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
